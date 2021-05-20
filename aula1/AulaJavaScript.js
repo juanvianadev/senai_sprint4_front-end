354dd68606585e65636b2a8d25db28a2737287f5
Juan <juanviana2577@gmail.com>
2021-05-20 13:09:22 -0300

    Doguinho Pronto

---
+++ aula1/AulaJavaScript.js
@@ -0,0 +1,7 @@
+console.log("10" / 2)
+
+console.log("10" + 2)
+
+console.log("10.2" / 2) //nunca usar "," pois ele interpreta como string sempre usar "."
+
+console.log(0.1 + 0.7) //ele não acha numeros precisos
